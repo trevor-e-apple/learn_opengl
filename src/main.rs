5494c2dc0ad43c1649fd6d852e0e2c89a21475b3
Trevor Apple <trappleco@gmail.com>
2025-11-10 12:21:05 -0800

    playing with uniforms

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
-use std::{ffi::c_void, mem::zeroed, ptr::null};
+use std::{
+    f64,
+    ffi::c_void,
+    mem::zeroed,
+    ptr::null,
+    time::{SystemTime, UNIX_EPOCH},
+};
 
 use glad_gl::gl::{self, GLchar, GLsizei, GLuint};
 use glfw::{self, Context, Key, OpenGlProfileHint, WindowEvent, WindowHint, WindowMode};
@@ -15,17 +21,10 @@ const VERTEX_SHADER_SOURCE: &str = concat!(
 const FRAGMENT_SHADER_ONE_SOURCE: &str = concat!(
     "#version 330 core\n",
     "out vec4 FragColor;\n",
+    "uniform vec4 ourColor;\n", // We set this variable in our opengl code
     "void main()\n",
     "{\n",
-    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n",
-    "}\n\0"
-);
-const FRAGMENT_SHADER_TWO_SOURCE: &str = concat!(
-    "#version 330 core\n",
-    "out vec4 FragColor;\n",
-    "void main()\n",
-    "{\n",
-    "   FragColor = vec4(1.0f, 1.0f, 0.0f, 1.0f);\n",
+    "   FragColor = ourColor;\n",
     "}\n\0"
 );
 
@@ -60,7 +59,7 @@ fn main() {
     // TODO: register resize callback
 
     // Create the shader program
-    let shader_programs = {
+    let shader_program = {
         // Build and compile our vertex shader
         let vertex_shader = unsafe {
             let vertex_shader = gl::CreateShader(gl::VERTEX_SHADER);
@@ -133,45 +132,8 @@ fn main() {
             fragment_shader
         };
 
-        let fragment_shader_two = unsafe {
-            let fragment_shader = gl::CreateShader(gl::FRAGMENT_SHADER);
-            let fragment_shader_source = {
-                let boxed_source_ptr = Box::new(FRAGMENT_SHADER_TWO_SOURCE.as_ptr());
-                Box::into_raw(boxed_source_ptr)
-            };
-            gl::ShaderSource(
-                fragment_shader,
-                1,
-                fragment_shader_source as *const *const GLchar,
-                null(),
-            );
-            gl::CompileShader(fragment_shader);
-
-            // Check for compilation errors
-            let mut success: i32 = 0;
-            let mut info_log: [u8; 512] = zeroed();
-            gl::GetShaderiv(
-                fragment_shader,
-                gl::COMPILE_STATUS,
-                &mut success as *mut i32,
-            );
-            if success == 0 {
-                gl::GetShaderInfoLog(
-                    fragment_shader,
-                    512,
-                    0 as *mut GLsizei,
-                    info_log.as_mut_ptr() as *mut GLchar,
-                );
-                eprintln!("Fragment shader compilation error");
-                eprintln!("Error info: {}", str::from_utf8(&info_log).unwrap());
-                panic!("panic");
-            }
-
-            fragment_shader
-        };
-
         // Create the shader program
-        let shader_program_one = unsafe {
+        let shader_program = unsafe {
             let shader_program = gl::CreateProgram();
 
             gl::AttachShader(shader_program, vertex_shader);
@@ -196,50 +158,19 @@ fn main() {
             shader_program
         };
 
-        let shader_program_two = unsafe {
-            let shader_program = gl::CreateProgram();
-
-            gl::AttachShader(shader_program, vertex_shader);
-            gl::AttachShader(shader_program, fragment_shader_two);
-            gl::LinkProgram(shader_program);
-
-            let mut success: i32 = 0;
-            let mut info_log: [u8; 512] = zeroed();
-            gl::GetProgramiv(shader_program, gl::LINK_STATUS, &mut success as *mut i32);
-            if success == 0 {
-                gl::GetProgramInfoLog(
-                    shader_program,
-                    512,
-                    0 as *mut GLsizei,
-                    info_log.as_mut_ptr() as *mut GLchar,
-                );
-                eprintln!("Shader program linking error");
-                eprintln!("Error info: {}", str::from_utf8(&info_log).unwrap());
-                panic!("panic");
-            }
-
-            shader_program
-        };
-
         unsafe {
             gl::DeleteShader(vertex_shader);
             gl::DeleteShader(fragment_shader_one);
-            gl::DeleteShader(fragment_shader_two);
         }
 
-        (shader_program_one, shader_program_two)
+        shader_program
     };
 
     // Vertex input
     let triangle_one: [f32; 9] = [
-        -0.5, 0.0, 0.0,
-        -1.0, -0.5, 0.0,
-        0.0, -0.5, 0.0,
-    ];
-    let triangle_two: [f32; 9] = [
-        0.5, 0.0, 0.0,
-        1.0, -0.5, 0.0,
-        0.0, -0.5, 0.0,
+        -0.5, 0.0, 0.0, // Vertex 1
+        -1.0, -0.5, 0.0, // Vertex 2
+        0.0, -0.5, 0.0, // Vertex 3
     ];
     let vaos = {
         let mut vbos: [GLuint; 2] = [0, 0];
@@ -274,29 +205,6 @@ fn main() {
                 0 as *const c_void,
             );
             gl::EnableVertexAttribArray(0);
-
-            // Bind vertex array object first
-            gl::BindVertexArray(vaos[1]);
-            // Set vbo type to ARRAY_BUFFER for vertices
-            gl::BindBuffer(gl::ARRAY_BUFFER, vbos[1]);
-
-            // Copy the data into the buffer
-            gl::BufferData(
-                gl::ARRAY_BUFFER,
-                (size_of::<f32>() * triangle_two.len()) as isize,
-                triangle_two.as_ptr() as *const c_void,
-                gl::STATIC_DRAW, // Data is set once and used many times
-            );
-
-            gl::VertexAttribPointer(
-                0,
-                3,
-                gl::FLOAT,
-                gl::FALSE,
-                (3 * size_of::<f32>()) as i32,
-                0 as *const c_void,
-            );
-            gl::EnableVertexAttribArray(0);
         }
         vaos
     };
@@ -312,12 +220,20 @@ fn main() {
         }
 
         unsafe {
-            gl::UseProgram(shader_programs.0);
-            gl::BindVertexArray(vaos[0]);
-            gl::DrawArrays(gl::TRIANGLES, 0, 3);
+            // clear the color buffer
+            gl::ClearColor(0.2, 0.3, 0.3, 1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT);
 
-            gl::UseProgram(shader_programs.1);
-            gl::BindVertexArray(vaos[1]);
+            gl::UseProgram(shader_program);
+            let uniform_location =
+                gl::GetUniformLocation(shader_program, "ourColor\0".as_ptr() as *const GLchar);
+            let green_value = {
+                let now = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
+                let now_millis = (2.0 * f64::consts::PI * (now.as_millis() as f64)) / 1000.0;
+                ((now_millis.sin() / 2.0) + 0.5) as f32
+            };
+            gl::Uniform4f(uniform_location, 0.0, green_value, 0.0, 1.0);
+            gl::BindVertexArray(vaos[0]);
             gl::DrawArrays(gl::TRIANGLES, 0, 3);
         }
 
